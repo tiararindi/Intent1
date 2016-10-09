c0a2df8a0e771dfc58a475f75408745372157291
Rindi Tiara PA <rinditiarapa@gmail.com>
2016-10-09 11:12:47 +0700

    startActivity Part 1

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
@@ -0,0 +1,15 @@
+package id.sch.smktelkom_mlg.learn.intent1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HasilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil);
+    }
+}
